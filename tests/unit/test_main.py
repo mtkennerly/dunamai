99781df391bcd07d4b7148a22a1976ba5f8b27da
Marius van Niekerk <marius.v.niekerk@gmail.com>
2020-12-02 21:33:31 +0000

    Fix test

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -19,6 +19,7 @@ def test__parse_args__from():
         tag_dir="tags",
         debug=False,
         bump=False,
+        tagged_metadata=False,
     )
     assert parse_args(["from", "git"]).vcs == "git"
     assert parse_args(["from", "mercurial"]).vcs == "mercurial"
@@ -37,6 +38,7 @@ def test__parse_args__from():
     assert parse_args(["from", "any", "--latest-tag"]).latest_tag is True
     assert parse_args(["from", "any", "--tag-dir", "foo"]).tag_dir == "foo"
     assert parse_args(["from", "any", "--debug"]).debug is True
+    assert parse_args(["from", "any", "--tagged-metadata"]).tagged_metadata is True
     assert parse_args(["from", "subversion", "--tag-dir", "foo"]).tag_dir == "foo"
 
     with pytest.raises(SystemExit):
