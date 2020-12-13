9cfe64fe30d212c17b35a7d0c37342e7eae3eadb
Aaron Fields <spirotot@gmail.com>
2020-12-13 09:46:00 -0500

    Use repo root URL for Subversion tags path building, instead of the URL for target.

--- dunamai/__init__.py
+++ dunamai/__init__.py
@@ -651,7 +651,7 @@ class Version:
         code, msg = _run_cmd("svn status")
         dirty = bool(msg)
 
-        code, msg = _run_cmd("svn info --show-item url")
+        code, msg = _run_cmd("svn info --show-item repos-root-url")
         url = msg.strip("/")
 
         code, msg = _run_cmd("svn info --show-item last-changed-revision")
